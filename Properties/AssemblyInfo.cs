f473bcbf0c78c5df41924892ed3ea99306e9ed0a
NicolasG <nico.homer@hotmail..com>
2024-03-25 15:57:20 -0300

    .

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2729")]
+[assembly: AssemblyVersion("1.0.0.2730")]
 [assembly: AssemblyFileVersion("2.0.2.4")]
